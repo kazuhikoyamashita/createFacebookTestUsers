1d542d86434430d22f4c0a6cc65625e8b892905a
Kazuhiko Yamashita <kazuhiko.yamashita@gmail.com>
2013-11-25 20:31:10 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+createFacebookTestUsers
+========================
+Facebookのテストユーザを作成します。
+
+利用方法
+------------
+* APP_IDにアプリIDを指定
+* APP_SECRETにシークレットキーを指定
+* createFacebookTestUsers.phpを実行
+
+ 
+ライセンス
+------------
+Copyright &copy; 2013 Kazuhiko Yamashita
+Distributed under the [MIT License][mit].
+
+[MIT]: http://www.opensource.org/licenses/mit-license.php
---
+++ createFacebookTestUsers.php
@@ -0,0 +1,40 @@
+<?php
+require_once('./curlPost.php');
+
+define(APP_ID, '');
+define(APP_SECRET, '');
+
+try {
+	$clientId = APP_ID;
+	$clientSecret = APP_SECRET;
+
+	if (empty($clientId) || empty($clientSecret)) {
+		throw new Exception("Error: Failed to get app id or secret key.");
+	}
+
+	// GET ACCESS TOKEN
+	$url = 'https://graph.facebook.com/oauth/access_token';
+	$params = array('client_id' => $clientId, 'client_secret' => $clientSecret, 'grant_type' => 'client_credentials');
+	$accessToken = curlPost($url, $params);
+	if ($accessToken == false) {
+		throw new Exception("Error: Failed to get access token.");
+	}
+	$accessToken = str_replace("access_token=", "", $accessToken);
+	
+	// CREATE TEST USERS
+	$url = 'https://graph.facebook.com/' . $clientId . '/accounts/test-users';
+	$params = array('installed' => 'true', 'permissions' => 'read_stream', 'method' => 'post', 'access_token' => $accessToken);
+	$json = curlPost($url, $params);
+	if ($json == false) {
+		throw new Exception("Error: Failed to create test users.");
+	}
+	$testUsers = json_decode($json);
+
+	echo "login_url: " . $testUsers->login_url . "\n";
+	echo "email: " . $testUsers->email . "\n";
+	echo "password: " . $testUsers->password . "\n";
+
+} catch (Exception $e) {
+	echo $e->getMessage() . "\n";
+}
+

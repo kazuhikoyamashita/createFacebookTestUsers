597394f8f14a1a992cc8527bd6494f3dbb895f4c
Kazuhiko Yamashita <kazuhiko.yamashita@gmail.com>
2014-01-07 21:25:06 +0900

    タブのスペース化

--- curlPost.php
+++ curlPost.php
@@ -1,33 +1,32 @@
 <?php
-function curlPost($url, $params)
-{
-	if (empty($url)) {		
-		return false;
-	}
-	
-	if (is_array($params)) {
-		$tmp = '';
-		foreach($params as $key => $value) {
-			if (!empty($tmp)) {
-				$tmp .= '&';
-			}
-			$tmp .= rawurlencode($key) . '=' . rawurlencode($value);
-		}
-		$params = $tmp;
-	}
-	
-	if (($params == null) || empty($params)) {
-		return false;
-	}
-	
-	$curl = curl_init($url);
-	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-	curl_setopt($curl, CURLOPT_HEADER, 0);
-	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); 
-	curl_setopt($curl, CURLOPT_POST, 1); 
-	curl_setopt($curl, CURLOPT_POSTFIELDS, $params); 
-	$result = curl_exec($curl);
-	
-	return $result;
+function curlPost($url, $params) {
+    if (empty($url)) {        
+        return false;
+    }
+
+    if (is_array($params)) {
+        $tmp = '';
+        foreach($params as $key => $value) {
+            if (!empty($tmp)) {
+                $tmp .= '&';
+            }
+            $tmp .= rawurlencode($key) . '=' . rawurlencode($value);
+        }
+        $params = $tmp;
+    }
+    
+    if (($params == null) || empty($params)) {
+        return false;
+    }
+    
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($curl, CURLOPT_HEADER, 0);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); 
+    curl_setopt($curl, CURLOPT_POST, 1); 
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $params); 
+    $result = curl_exec($curl);
+    
+    return $result;
 }
